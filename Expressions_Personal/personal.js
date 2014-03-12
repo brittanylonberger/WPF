ecd2563e61fb63a637360046a34e01d9b4c1544e
brittanylonberger <brittany.l@fullsail.edu>
2014-03-11 23:25:21 -0400

    added comments
    added comments to explain the expression I am going to solve.

--- Expressions_Personal/personal.js
+++ Expressions_Personal/personal.js
@@ -1,3 +1,4 @@
 <script type="text/javascript">
-// 
+// This code will calculate the cost of laundry soap over the course of a year. 
+// Equation is (AmountofSoap(oz) p/load * Loads p/week / Total(oz) p/bottle) * Cost p/bottle * 52 weeks p/year
 
