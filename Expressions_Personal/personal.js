6eae10fd5553364994b0fab9a39e0f2a164ed656
brittanylonberger <brittany.l@fullsail.edu>
2014-03-11 23:37:30 -0400

    added code
    Added code to define variables. Test pending.

--- Expressions_Personal/personal.js
+++ Expressions_Personal/personal.js
@@ -4,5 +4,12 @@
 
 document.bgColor = "#FF94DB";
 
+var soapAmnt = prompt("Enter the amount of soap (in ounces) that you use per load of laundry.", 5);
+var loadWeek = prompt("Enter the total number of loads of laundry you do per week.", 7);
+
+var detCost = 11.97;
+var totOz = 100;
+
+
 
 
