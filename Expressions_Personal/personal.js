33c8611bcc37f24d05b934e2d6653aac8e5f76c9
brittanylonberger <brittany.l@fullsail.edu>
2014-03-12 00:02:45 -0400

    added alert to display answer. test pending

--- Expressions_Personal/personal.js
+++ Expressions_Personal/personal.js
@@ -8,11 +8,15 @@ var soapAmnt = prompt("Enter the amount of soap (in ounces) that you use per loa
 var loadWeek = prompt("Enter the total number of loads of laundry you do per week.", 7);
 
 var detCost = 11.97;
-var totOz = 100;
+var launTot = 100;
 
 var step1;
-step1 = soapAmnt * loadWeek;
-alert(step1);
+step1 = soapAmnt * loadWeek / launTot * detCost * 52;
+myInt;
+
+
+
+alert(" It looks like you spend approximately " + " $ " + step1 + " per year on laundry detergent. Wow! ");
 
 
 
