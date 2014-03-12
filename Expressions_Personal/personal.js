252e484bc9ac053a0316513193ecc2c7a3d28905
brittanylonberger <brittany.l@fullsail.edu>
2014-03-11 23:32:49 -0400

    added background color to document

--- Expressions_Personal/personal.js
+++ Expressions_Personal/personal.js
@@ -1,5 +1,8 @@
 // This code will calculate the cost of laundry soap over the course of a year. 
 // Equation is (AmountofSoap(oz) p/load * Loads p/week / Total(oz) p/bottle) * Cost p/bottle * 52 weeks p/year.
+// Cost and size of laundry detergent is based on information obtained from WalMart.com "http://www.walmart.com/ip/Tide-2x-Ultra-High-Efficiency-Liquid-Laundry-Detergent-Original-Scent-100-fl-oz/11045842".
+
+document.bgColor = "#FF94DB";
 
 
 
