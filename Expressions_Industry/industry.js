c37006873a0cd3df1c9e84248a5959e289efb320
brittanylonberger <brittany.l@fullsail.edu>
2014-03-13 19:59:32 -0400

    redefined project

--- Expressions_Industry/industry.js
+++ Expressions_Industry/industry.js
@@ -1,7 +1,7 @@
 // Industry .js
+// This script will calculate the aspect ratio of a resized image based on the wanted height in order to maintain image consistency. 
 
-var industryArray = new Array();
-	industryArray[0] = "
-	industryArray[1] = 
-	industryArray[2] = 
-	industryArray[3] = 
+var aspectRatio = new Array(origHeight, origWidth, newWidth);
+
+var
+
