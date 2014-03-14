c24656478734c24e9477d5b91a2bd902fb1b3854
brittanylonberger <brittany.l@fullsail.edu>
2014-03-13 20:14:29 -0400

    correcting errors

--- Expressions_Industry/industry.js
+++ Expressions_Industry/industry.js
@@ -2,13 +2,13 @@
 // This script will calculate the aspect ratio of a resized image based on the wanted height in order to maintain image consistency. 
 // Formula is: original height / original width x new width = new height
 
-var aspectRatio = new Array(firstHeight, origWidth, newWidth);
+var aspectRatio = new Array();
 
 	aspectRatio[0] = "800";
 	aspectRatio[1] = "600";
 	aspectRatio[2] = "300";
 
-var newHeight = "(aspectRatio[0] / aspectRatio[1]) x aspectRatio[2]";
+var newHeight = (aspectRatio[0] / aspectRatio[1]) x aspectRatio[2];
 console.log(newHeight);
 
 
