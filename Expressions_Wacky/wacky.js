d7caba84f59eede2e7aa34dd210f3192e216691b
brittanylonberger <brittany.l@fullsail.edu>
2014-03-13 22:39:30 -0400

    began adding parameters for conversion

--- Expressions_Wacky/wacky.js
+++ Expressions_Wacky/wacky.js
@@ -1,1 +1,13 @@
 // wacky.js
+// Formula to convert unicorns to fairies.
+// Formula is: weight of unicorn * 3.14 / megillagrams of magic inside of the unicorns horn + number of hairs inside unicorn's left nostril.
+
+
+var uniWeight = "100";
+
+console.log("If your unicorn weighs " + uniWeight + " pounds..");
+
+var megMil = "2300";
+
+console.log("And your unicorn's horn contains " + megMil + " megillagrams of magic. ");
+
