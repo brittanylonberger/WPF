98aba9908c4f48e6f17242de28eec8d9d53f8807
brittanylonberger <brittany.l@fullsail.edu>
2014-03-13 22:41:14 -0400

    debugged and added parameters

--- Expressions_Wacky/wacky.js
+++ Expressions_Wacky/wacky.js
@@ -3,11 +3,12 @@
 // Formula is: weight of unicorn * 3.14 / megillagrams of magic inside of the unicorns horn + number of hairs inside unicorn's left nostril.
 
 
-var uniWeight = "100";
+var uniWeight = 100;
 
 console.log("If your unicorn weighs " + uniWeight + " pounds..");
 
 var megMil = "2300";
 
-console.log("And your unicorn's horn contains " + megMil + " megillagrams of magic. ");
+console.log("And your unicorn's horn contains " + megMil + " megillagrams of magic.. ");
 
+var uniHair = 17000;
