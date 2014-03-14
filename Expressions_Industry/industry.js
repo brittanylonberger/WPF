030fc897839ffb1a8f208b045b1cfc03cd70deb7
brittanylonberger <brittany.l@fullsail.edu>
2014-03-13 20:03:23 -0400

    defined variables of aspectRatio

--- Expressions_Industry/industry.js
+++ Expressions_Industry/industry.js
@@ -1,7 +1,12 @@
 // Industry .js
 // This script will calculate the aspect ratio of a resized image based on the wanted height in order to maintain image consistency. 
+// Formula is: original height / original width x new width = new height
 
 var aspectRatio = new Array(origHeight, origWidth, newWidth);
 
-var
+	aspectRatio[0] = 800;
+	aspectRatio[1] = 600;
+	aspectRatio[2] = 300;
+
+
 
