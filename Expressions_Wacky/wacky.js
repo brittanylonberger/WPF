c4ae38306bc20489e842556390ae200767d16211
brittanylonberger <brittany.l@fullsail.edu>
2014-03-13 22:49:35 -0400

    completed script

--- Expressions_Wacky/wacky.js
+++ Expressions_Wacky/wacky.js
@@ -19,3 +19,7 @@ console.log("And your unicorn's left nostril contains " + uniHair + " hairs.. ")
 var fairyConv;
 fairyConv = uniWeight * pi / megMil + uniHair;
 
+var myRound = parseInt(fairyConv);
+
+console.log("With the amount of unicorn you have, you can convert that to " + myRound + "fairies.");
+
