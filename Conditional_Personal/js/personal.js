8f5eabc04fa0d41401b5291c3734d9777355879b
brittanylonberger <brittany.l@fullsail.edu>
2014-03-19 10:31:26 -0400

    added a variable for child's weight and edited comments to include info requested by professor

--- Conditional_Personal/js/personal.js
+++ Conditional_Personal/js/personal.js
@@ -1,3 +1,4 @@
-// Personal js
+// Brittany Lonberger; March 19, 2014; Personal Condiditional Assignment
 // This script will determine the size of diapers to buy for a child based on the weight of the child. 
- 
+
+var child = prompt("Please enter your child's weight /(in pounds/)") 
