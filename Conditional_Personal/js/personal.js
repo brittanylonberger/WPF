04ab4ed82c659ced6d7dc5f7bd69aff4d9f06aef
brittanylonberger <brittany.l@fullsail.edu>
2014-03-19 11:41:24 -0400

    identified variables for equation

--- Conditional_Personal/js/personal.js
+++ Conditional_Personal/js/personal.js
@@ -1,2 +1,8 @@
 // Brittany Lonberger; March 19, 2014; Personal Condiditional Assignment
+// This equation with a conditional will answer the question, "Can I afford this car?"
 
+var carPrice = prompt("How much is the total cost of the car you are looking to buy?");
+
+var downPmnt = prompt("What are you going to pay for your initial down payment?");
+
+var loanLife = prompt("What is the life of the loan you will recieve /(in months/)?")
