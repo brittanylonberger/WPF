6cda6ca0b3e2cc5f1e5567283f36d59eb91eb66e
brittanylonberger <brittany.l@fullsail.edu>
2014-03-19 14:05:13 -0400

    added conditions to all variables

--- Conditional_Personal/test.js
+++ Conditional_Personal/test.js
@@ -2,7 +2,7 @@
 // This equation with a conditional will answer the question, "Can I afford this car?"
 
 
-// Car price variable..
+// Total car price information...
 var _carPrice;
 // To make sure a variable was entered correctly for the price of the car....
 function carPrice(){
@@ -19,9 +19,10 @@ function carPrice(){
 carPrice();
 console.log("$" + _carPrice + " is the total cost of the car.");
 
-var _downPmnt;
 
-// To make sure a variable was entered correctly for the down payment amount...
+// Down payment information...
+var _downPmnt;
+// To make sure a variable was entered correctly for the down payment on the car....
 function downPmnt(){
 	_downPmnt = prompt("What are you going to pay for your initial down payment?");
 	
@@ -36,9 +37,26 @@ function downPmnt(){
 downPmnt();
 console.log("$" + _downPmnt + " is your down payment.");
 
-var _loanLife = prompt("What is the life of the loan you will recieve, (in months)?");
-console.log(loanLife + " is the life of your loan in months.");
 
+// Loan life information...
+var _loanLife();
+// To make sure the variable was entered correctly for the loan life...
+function downPmnt(){
+	_loanLife = prompt("What is the life of the loan you will recieve, (in months)?");
+	
+	if(_loanLife == "" || !parseInt(_loanLife)){ // Again, making sure the entered value is a number and is entered...
+		alert("It looks like you've entered something incorrectly... Please try again.")
+	} else {
+		return _loanLife;
+	}
+
+}
+
+loanLife();
+console.log(_loanLife + " is the life of your loan in months.");
+
+
+// The final step.. The cost of your monthly payments!
 var total = (_carPrice - _downPmnt) / _loanLife;
 alert("You would have to make monthly payments of $" + total);
 console.log("You would have to make monthly payments of $" + total);
