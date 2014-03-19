ac717e46f2b5bf2031d8ce5ed1ff81ad1f1b9122
brittanylonberger <brittany.l@fullsail.edu>
2014-03-19 11:51:23 -0400

    added console.log to all variables.

--- Conditional_Personal/js/personal.js
+++ Conditional_Personal/js/personal.js
@@ -2,7 +2,13 @@
 // This equation with a conditional will answer the question, "Can I afford this car?"
 
 var carPrice = prompt("How much is the total cost of the car you are looking to buy?");
+console.log("$" + carPrice + " is the total cost of the car.")
 
 var downPmnt = prompt("What are you going to pay for your initial down payment?");
+console.log("$" + downPmnt + " is your down payment.")
 
-var loanLife = prompt("What is the life of the loan you will recieve /(in months/)?")
+var loanLife = prompt("What is the life of the loan you will recieve, (in months)?")
+console.log(loanLife + " is the life of your loan in months.")
+
+var total = alert(carPrice - downPmnt / loanLife + "are your monthly payments");
+console.log("$" + total + " is the payments you would have to make monthly.")
