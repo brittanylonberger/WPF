c38caffb3f9752515699650c11c32f1b19608ed8
brittanylonberger <brittany.l@fullsail.edu>
2014-03-19 13:40:16 -0400

    adjusting code to allow for validation of input

--- Conditional_Personal/test.js
+++ Conditional_Personal/test.js
@@ -3,11 +3,11 @@
 
 var _carPrice;
 
-// Recursive function until a value is entered
+// To make sure a variable was entered correctly for the price of the car....
 function carPrice(){
 	_carPrice = prompt("How much is the total cost of the car you are looking to buy?");
 
-	if(_carPrice == "" || !parseInt(_carPrice)){ // Make sure it's a number too..
+	if(_carPrice == "" || !parseInt(_carPrice)){ // Gotta make sure it's actually a number...
 		alert("It looks like you entered something incorrectly.. Please try again.");
 		carPrice();
 	} else {
@@ -19,18 +19,24 @@ carPrice();
 
 console.log("$" + _carPrice + " is the total cost of the car.");
 
-var downPmnt = prompt("What are you going to pay for your initial down payment?");
-console.log("$" + downPmnt + " is your down payment.");
+var _downPmnt;
 
-var loanLife = prompt("What is the life of the loan you will recieve, (in months)?");
+// To make sure a variable was entered correctly for the down payment amount...
+function downPmnt(){
+	_downPmnt = prompt("What are you going to pay for your initial down payment?");
+
+}
+console.log("$" + _downPmnt + " is your down payment.");
+
+var _loanLife = prompt("What is the life of the loan you will recieve, (in months)?");
 console.log(loanLife + " is the life of your loan in months.");
 
-var total = _carPrice - downPmnt / loanLife;
-alert(total);
+var total = _carPrice - _downPmnt / _loanLife;
+alert("You would have to make monthly payments of $" + total);
 console.log("You would have to make monthly payments of $" + total);
 
-if(total > 250){
+if(total >= 250){
 	alert("It seems that you will not be able to afford this car.");
 }else {
-	alert("This car is totally affordable!")
+	alert("This car is totally affordable!");
 }
