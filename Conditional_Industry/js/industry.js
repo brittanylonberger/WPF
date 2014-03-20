d0b0b9d9516d7571a5e74656a691a7d5af0e2a63
brittanylonberger <brittany.l@fullsail.edu>
2014-03-20 10:38:39 -0400

    altered code to allow user to input their own salary requirements

--- Conditional_Industry/js/industry.js
+++ Conditional_Industry/js/industry.js
@@ -5,7 +5,7 @@
 var payRate;
 // Ensure that the user entered values are valid
 function _payRate(){
-	payRate = prompt("Enter the hourly rate at which you will receive pay.");
+	payRate = prompt("Enter the hourly rate at which you will receive pay. (in dollars)");
 	
 	if(payRate == "" || !parseFloat(payRate)){ // To make sure the inputted data is a number
 		alert("I think something you entered may be incorrect... Please try again.");
@@ -39,5 +39,27 @@ console.log("You are expecting to work " + weekHours + " hours per week.")
 var total = parseInt(payRate * weekHours * 52);
 console.log("It looks like you will make approximately $" + total + " per year in this position.")
 alert("It looks like you will make approximately $" + total + " per year in this position.");
+
+// Identify goal salary 
+var goalSal;
+// Ensure that the user enters valid values
+function _goalSal(){
+	goalSal = prompt("What are your minumum salary requirements? (in dollars)");
+	
+	if(goalSal == "" || !parseFloat(goalSal)){
+		alert("I think you may have entered something incorrectly... Please try again.");
+		_goalSal();
+	} else {
+		return goalSal;
+	}
+}
+// Run the salary goal prompt
+_goalSal();
+console.log("So, you're saying that you need to make $" + goalSal + " per year.")
+
+// The final conditional.
+if(total < goalSal){
+	alert("Uh, oh.. This position's pay rate does not meet your needs.");
+}
 
 
