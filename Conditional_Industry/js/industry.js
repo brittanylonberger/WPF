189ec16270b4ce3034b6c1edd864811ef857d705
brittanylonberger <brittany.l@fullsail.edu>
2014-03-20 09:33:48 -0400

    began to define variables and functions for hourly pay rate

--- Conditional_Industry/js/industry.js
+++ Conditional_Industry/js/industry.js
@@ -1,3 +1,10 @@
 // Brittany Lonberger; March 20, 2014; Industry Conditional
-// This script is designed to determine if a potential job will provide an acceptable salary based on the user's need.
+// This script is designed to determine if a potential job will provide an acceptable salary based on hourly pay rate, hours worked per week, and weeks per year.
 
+// Identify the hourly pay rate
+var payRate;
+// Ensure that the user entered values are valid
+function _payRate(){
+	payRate = prompt("Enter the hourly rate at which you will receive pay.");
+	
+}
