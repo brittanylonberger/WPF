e39fb5d091b15792cc31b599478bb63fd63d1006
brittanylonberger <brittany.l@fullsail.edu>
2014-03-20 20:37:42 -0400

    added pack of gum variable and log

--- Conditional_Wacky/js/wacky.js
+++ Conditional_Wacky/js/wacky.js
@@ -1,1 +1,21 @@
 // Brittany Lonberger; March 20, 2014; Wacky conditional
+// This script will answer the question, "Did I bring enough gum for everyone?"
+
+// Identify amount of gum variable
+var packGum;
+// Validation
+function _packGum(){
+	packGum = prompt("How many packs of gum do you have?");
+	
+	if(packGum == "" || !parseInt(packGum));
+	alert("I know you have some gum.. go back and tell me how much!");
+	_packGum;
+	} else {
+		return packGum;
+	}
+}
+
+// run pack gum
+_packgum;
+console.log("Ok, so you have " + packGum + " pack(s) of gum.");
+
