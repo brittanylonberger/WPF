6b4fa099551bd76f2661f02b9a7ab8c50152e1e2
brittanylonberger <brittany.l@fullsail.edu>
2014-03-26 14:10:49 -0400

    added comments and debugged

--- Functions_Personal/js/personal.js
+++ Functions_Personal/js/personal.js
@@ -3,22 +3,28 @@
 // March 26, 2014
 // This will calculate your monthly budget based on monthly income and monthly expenses.
 
-
+// This function will identify the user's monthly income and monthly expenses
 function _monBudg(){
-	var monIn = prompt("Please input your average monthly income in a whole dollar amount.");
+	// monthly income (monIn)
+	var monIn = prompt("Please input your average monthly income in a whole dollar amount."); 
+	// monthly expenses (monEx)
 	var monEx = prompt("Please input your average monthly expenses in a whole dollar amount.");
 	
+	// making sure the input of monIn is a valid number
 	if (monIn == "" || !parseFloat(monIn)) {
 		alert("It looks like your input was in an incorrect format. Please go back and try again.");
 		_monBudg();
 	} else{
+		// print monIn to console permitting valid input
 		console.log("Your monthly income is $" + monIn);
-		return;}
+	}
 
+	// making sure the input of monEx is a valid number
 	if (monEx == "" || !parseFloat(monEx)){
 		alert("It looks like your input was in an incorrect format. Please go back and try again.");
 		_monBudg();
 	} else {
+		// print monEx to console permitting valid input
 		console.log("Your monthly expenses total $" + monEx);
 		return;
 	}
