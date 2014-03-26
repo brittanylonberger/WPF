3641161ac87b923342b9fc78145fc97fcf50b04a
brittanylonberger <brittany.l@fullsail.edu>
2014-03-26 13:22:01 -0400

    added main variables (monthly income and expenses) within the function

--- Functions_Personal/js/personal.js
+++ Functions_Personal/js/personal.js
@@ -1,3 +1,11 @@
 //Personal Function
 // Brittany Lonberger
 // March 26, 2014
+// This will calculate your monthly budget based on monthly income and monthly expenses.
+
+function monBudg(){
+	var monIn = prompt("Please input your monthly income in a dollar amount.");
+	var monEx = prompt("Please input your monthly expenses in a dollar amount.");
+}
+
+monBudg();
