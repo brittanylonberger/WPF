015baf44abd48ecad75cb3e4609ed1366d017fa2
brittanylonberger <brittany.l@fullsail.edu>
2014-03-26 13:33:27 -0400

    debug

--- Functions_Personal/js/personal.js
+++ Functions_Personal/js/personal.js
@@ -3,14 +3,14 @@
 // March 26, 2014
 // This will calculate your monthly budget based on monthly income and monthly expenses.
 
-var monIn;
-var monEx;
 
-function _monBudg(){
-	monIn = prompt("Please input your average monthly income (in dollars).");
-	monEx = prompt("Please input your average monthly expenses (in dollars).");
+var monIn = prompt("Please input your average monthly income (in dollars).");
+var monEx = prompt("Please input your average monthly expenses (in dollars).");
 	
-	if (monIn == "" || !parseFloat(monIn, monEx)) {
+
+function _monBudg(monIn, monEx){
+	
+	if (monIn, monEx == "" || !parseFloat(monIn, monEx)) {
 		alert("It looks like your input was in an incorrect format. Please go back and try again.");
 		_monBudg();
 	} else{
