c7fa18cb2a7e2bf70dc2cf0549eccdc16f89a2aa
brittanylonberger <brittany.l@fullsail.edu>
2014-03-26 15:10:33 -0400

    minor changes and alterations and finishing script

--- Functions_Personal/js/personal.js
+++ Functions_Personal/js/personal.js
@@ -7,8 +7,6 @@
 function _monBudg(){
 	// monthly income (monIn)
 	var monIn = prompt("Please input your average monthly income in a whole dollar amount."); 
-	// monthly expenses (monEx)
-	var monEx = prompt("Please input your average monthly expenses in a whole dollar amount.");
 	
 	// making sure the input of monIn is a valid number
 	if (monIn == "" || !parseFloat(monIn)) {
@@ -19,6 +17,8 @@ function _monBudg(){
 		console.log("Your monthly income is $" + monIn);
 	}
 
+	// monthly expenses (monEx)
+	var monEx = prompt("Please input your average monthly expenses in a whole dollar amount.");
 	// making sure the input of monEx is a valid number
 	if (monEx == "" || !parseFloat(monEx)){
 		alert("It looks like your input was in an incorrect format. Please go back and try again.");
@@ -26,9 +26,18 @@ function _monBudg(){
 	} else {
 		// print monEx to console permitting valid input
 		console.log("Your monthly expenses total $" + monEx);
-		return;
 	}
-
+	
+	// calculate the amount of income left over after monIn - monEx
+	var leftOver = monIn - monEx;
+	
+	// can you afford your monthly expenses?
+	var _result = leftOver > 0 ? true : false;
+	if(_result)
+	console.log("You should be able to manage your monthly expenses. Yay!");
+	else 
+	console.log("I think you've bit off more than you can chew on expenses..");
+	return;
 }
 
 // run the budget calculating funtion
