780d6240d131c3f4850a049b5f1f6c7b4fd241c1
brittanylonberger <brittany.l@fullsail.edu>
2014-03-26 13:56:13 -0400

    more debugging

--- Functions_Personal/js/personal.js
+++ Functions_Personal/js/personal.js
@@ -8,14 +8,14 @@ function _monBudg(){
 	var monIn = prompt("Please input your average monthly income (in dollars).");
 	var monEx = prompt("Please input your average monthly expenses (in dollars).");
 	
-	if (monIn == "" || !parseFloat(monIn)) {
+	if (monIn == "" || !parseFloat(monIn) || !parseInt(monIn)) {
 		alert("It looks like your input was in an incorrect format. Please go back and try again.");
 		_monBudg();
 	} else{
 		console.log("Your monthly income is $" + monIn);
 	}
 
-	if (monEx == "" || !parseFloat(monEx)){
+	if (monEx == "" || !parseFloat(monEx) || !parseInt(monEx)){
 		alert("It looks like your input was in an incorrect format. Please go back and try again.");
 		_monBudg();
 	} else {
